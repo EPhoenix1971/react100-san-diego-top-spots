8c21ba1a40f79ddc5b7a53f71eb26084ef40c3aa
Emily Price <emilyprice@Emilys-MacBook-Pro.local>
2019-09-22 14:13:16 -0700

    submit without fancy style

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import TopSpot from './topspot';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topspots: []
+    }
+  }
+
+  componentWillMount() {
+    axios
+    .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+    .then(response => response.data)
+    .then(topspots => this.setState({ topspots }));
+  }
+
+  
+    
   render() {
+
     return (
-      <div className='App'></div>
+      <div className='App'>
+          <div className='container'>
+            <div className="jumbotron">
+              <h1 className="text">San Diego Top Spots</h1>
+              <h4 className="text">A list of the top 30 places to see in San Diego, California</h4>
+            </div>
+            
+        {
+          this.state.topspots.map(topspot => (
+          <TopSpot
+            key={ topspot.id }
+            name={ topspot.name }
+            description={ topspot.description }
+            location={ topspot.location } 
+          />
+          ))
+        }
+          </div>      
+      </div>
     );
   }
+  
 }
 
 export default App;
---
+++ src/topspot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => (
+  <div className='well'>
+    <h4>{props.name}</h4>
+    <p>{props.description}</p>
+    <button type="button" className="btn btn-info">
+      <a href={"https://www.google.com/maps?q=" + props.location}>Link to Google Maps</a>
+    </button>
+  </div> 
+
+);
